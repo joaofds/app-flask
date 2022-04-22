4471c0b08e1380a549f7ca9825b8744518059491
Joao <jo4o.fds@gmail.com>
2022-04-22 00:15:28 -0300

    flask app

---
+++ src/routes.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, render_template, request
+
+site_bp = Blueprint('site_page', __name__, template_folder='templates')
+
+@site_bp.route('/', methods=['GET'])
+def index():
+    title = 'Seja bem vindo'
+    return render_template('index.html', title=title)
+
+@site_bp.route('/about', methods=['GET'])
+def about():
+    return render_template('about_us.html')
+
+@site_bp.route('/contact', methods=['GET'])
+def contact():
+    title = 'Contato'
+    return render_template('contact.html', title = title)
